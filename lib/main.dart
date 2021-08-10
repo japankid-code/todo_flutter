a9f2f6c8ea4d57459d809be72a9f70efbb57c2d3
japankid-code <japankid.jake@gmail.com>
2021-08-09 19:22:11 -0500

    add hearts and heart page

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      theme: ThemeData(
+          primarySwatch: Colors.grey,
+          primaryColor: Colors.grey[900],
+          accentColor: Colors.green[300],
+          scaffoldBackgroundColor: Colors.grey[100],
+          textTheme: TextTheme(
+              headline1: TextStyle(
+                color: Colors.grey[100],
+                fontSize: 22,
+                fontFamily: 'RobotoMono',
+              ),
+              bodyText2: TextStyle(
+                color: Colors.grey[100],
+                fontSize: 22,
+                fontFamily: 'Exo2',
+              ))),
       home: Scaffold(
         body: Center(
           child: RandomWords(),
@@ -23,15 +38,47 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[]; // NEW
-  final _biggerFont = const TextStyle(fontSize: 18);
+  final List<WordPair> _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute<void>(
+      // NEW lines from here...
+      builder: (BuildContext context) {
+        final tiles = _saved.map(
+          (WordPair pair) {
+            return ListTile(
+              title: Text(
+                pair.asPascalCase,
+              ),
+            );
+          },
+        );
+        final divided = tiles.isNotEmpty
+            ? ListTile.divideTiles(context: context, tiles: tiles).toList()
+            : <Widget>[];
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Saved Suggestions'),
+          ),
+          body: ListView(children: divided),
+        );
+      },
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // Add from here...
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
+          title: Text(
+            'Startup Name Generator',
+          ),
+          actions: [
+            IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+          ]),
       body: _buildSuggestions(),
     );
   }
@@ -39,30 +86,17 @@ class _RandomWordsState extends State<RandomWords> {
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16),
-        // The itemBuilder callback is called once per suggested
-        // word pairing, and places each suggestion into a ListTile
-        // row. For even rows, the function adds a ListTile row for
-        // the word pairing. For odd rows, the function adds a
-        // Divider widget to visually separate the entries. Note that
-        // the divider may be difficult to see on smaller devices.
         itemBuilder: (BuildContext _context, int i) {
-          // Add a one-pixel-high divider widget before each row
-          // in the ListView.
           if (i.isOdd) {
+            // For odd rows, the function adds a one-pixel-high Divider widget
             return Divider();
           }
-
-          // The syntax "i ~/ 2" divides i by 2 and returns an
-          // integer result.
-          // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
           // This calculates the actual number of word pairings
-          // in the ListView,minus the divider widgets.
+          // in the ListView, minus the divider widgets.
           final int index = i ~/ 2;
-          // If you've reached the end of the available word
-          // pairings...
           if (index >= _suggestions.length) {
-            // ...then generate 10 more and add them to the
-            // suggestions list.
+            // If you've reached the end of the available word pairings
+            // ...then generate 10 more and add them to the suggestions list.
             _suggestions.addAll(generateWordPairs().take(10));
           }
           return _buildRow(_suggestions[index]);
@@ -70,11 +104,23 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair); // NEW
     return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-    );
+        title: Text(
+          pair.asPascalCase,
+        ),
+        trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+        });
   }
 }
